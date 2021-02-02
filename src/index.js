2820be737095105bf51cb89ae1a2af82d56d5782
apotapov <apotapovxx@mail.ru>
2021-02-02 22:51:40 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const bracketPairs = bracketsConfig.map(function (val) {
+        return val.join("");
+    });
+
+    let reducedStr = str;
+    let prevStr;
+    do{
+        prevStr = reducedStr;
+        bracketPairs.forEach(function (val) {
+            reducedStr = reducedStr.replace(val,"");
+        });
+    } while (reducedStr.length < prevStr.length);
+
+    return reducedStr.length === 0;
 }
